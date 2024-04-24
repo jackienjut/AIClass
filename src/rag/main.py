f5ba9a2219d1a3282ed86c077ab6fdb2bd5deda4
jackienjut <luhaiming86@163.com>
2024-04-24 22:09:50 +0800

    add

--- src/rag/main.py
+++ src/rag/main.py
@@ -10,3 +10,16 @@ warnings.simplefilter("ignore") # 屏蔽 ES 的一些Warnings
 
 nltk.download('punkt')  # 英文切词、词根、切句等方法
 nltk.download('stopwords')  # 英文停用词库
+
+def to_keywords(input_string):
+    """
+    将输入字符串转换为关键字列表
+    """
+    no_symbols = re.sub(r'[^a-zA-Z0-9\s]', '', input_string)
+    word_tokens =word_tokenize(no_symbols)
+
+    stop_words = set(stopwords.words('english'))
+
+    ps = PorterStemmer()
+    filter_sentence = [ps.stem(w) for w in word_tokens if not w in stop_words]
+    return ''.join(filter_sentence)
